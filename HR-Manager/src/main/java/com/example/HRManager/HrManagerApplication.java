8d35ac9d2acb4158ea2ba455dd99ab64ff906cf3
TuanAnh <vzzederzv@gmail.com>
2021-12-20 10:40:17 +0700

    Initial commit

---
+++ HR-Manager/src/main/java/com/example/HRManager/HrManagerApplication.java
@@ -0,0 +1,13 @@
+package com.example.HRManager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HrManagerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HrManagerApplication.class, args);
+	}
+
+}
